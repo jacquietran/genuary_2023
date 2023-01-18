401872967d6aa636fe77132c698f18db26da3ab6
Jacquie Tran <jacquie.tran@gmail.com>
2023-01-19 00:05:12 +1300

    Added day 19 piece

--- code/20230119.R
+++ code/20230119.R
@@ -1,38 +1,48 @@
-library(ggfx)
+# Load libraries ---------------------------------------------------------------
+
+library(paintr)
 library(ggplot2)
+library(magick)
 
-cos_wave <- function(width, height) {
-  x <- matrix(0, ncol = width, nrow = height)
-  x <- cos(col(x)/100)
-  as.raster((x + 1) / 2)}
+# Import image -----------------------------------------------------------------
 
-p <- ggplot() +
-  as_reference(
-    cos_wave,
-    id = "wave") +
-  with_variable_blur(
-    geom_point(aes(disp, mpg), mtcars, size = 4),
-    x_sigma = ch_red("wave"),
-    y_sigma = ch_alpha("wave"),
-    angle = ch_red("wave"),
-    x_scale = 15,
-    y_scale = 15,
-    angle_range = c(-45, 45)) +
-  geom_point(aes(disp, mpg), mtcars, size = 3, alpha = 0.7) +
-  coord_cartesian(expand = FALSE) +
+img <- here::here("img/ingredients/jean-beller-6cLBliGpaoM-unsplash.jpg")
+
+# Generate paint-by-numbers data
+painted <- paint_by_numbers(
+  img,
+  brightness = 170, saturation = 200, simplify = 0.1,
+  target_palette = c("#FFFFFF", "#F8F9FA", "#DEE2E6", "#ADB5BD", "#495057", "#212529", "#000000"))
+
+# Build plot -------------------------------------------------------------------
+
+p <- painted$painted_picture +
   theme_void() +
   theme(
+    legend.key = element_blank(),
+    legend.position = "none",
     plot.background = element_rect(fill = "#FFFFFF", colour = "#FFFFFF"))
-  
+
+# Save to file -----------------------------------------------------------------
+
 ggsave(
-  here::here("img/ingredients/20230119.png"), p,
-  width = 3, height = 3, units = "in", dpi = 600)
+  here::here("img/ingredients/20230119_base.png"), p,
+  width = 12, height = 10, units = "in", dpi = 600)
 
-beepr::beep(5)
+# Shift to {magick} workflow ---------------------------------------------------
 
-ggplot() +
-  geom_point(aes(disp, mpg), mtcars, size = 15, shape = "\u259b")
+base <- image_read(here::here("img/ingredients/20230119_base.png"))
 
-# point types:
-# https://unicode-table.com/en/blocks/block-elements/
-# https://unicode-table.com/en/blocks/geometric-shapes/
+base_cropped <- base |>
+  image_crop("7200x5500") |>
+  image_trim(fuzz = 50) |>
+  image_morphology(method = "Erode", kernel = "Disk", iterations = 4) |>
+  image_noise(noisetype = "Gaussian") |>
+  image_noise(noisetype = "Gaussian") |>
+  image_noise(noisetype = "Laplacian") |>
+  image_border("#FFFFFF", "800x800") |>
+  image_scale("6000x5000!")
+
+image_write(
+  image = base_cropped, path = here::here("img/20230119.png"))
+
