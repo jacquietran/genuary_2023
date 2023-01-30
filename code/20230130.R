795c0632cd910456fe81983a9ec60b6e604e9158
Jacquie Tran <jacquie.tran@gmail.com>
2023-01-30 13:12:36 +1300

    Added piece for day 30

--- code/20230130.R
+++ code/20230130.R
@@ -2,93 +2,166 @@
 
 library(dplyr)
 library(ggplot2)
-# Also requires {tibble}, {scales}, {ggfx}
-
-# Define custom function -------------------------------------------------------
-
-blotchy <- function(seed, n_points, x_min, x_max, y_min, y_max, palette){
-  
-  if(missing(n_points)){
-    n_points <- 1000
-  }
-  
-  if(missing(x_min)){
-    x_min <- 0
-  }
-  
-  if(missing(x_max)){
-    x_max <- 1
-  }
-  
-  if(missing(y_min)){
-    y_min <- 0
-  }
-  
-  if(missing(y_max)){
-    y_max <- 1
-  }
-  
-  # Requires {tibble}, {scales}, {dplyr}
-  
-  set.seed(seed)
-  df <- tibble::tibble(
-    x = scales::rescale(rnorm(n_points), to = c(x_min, x_max)),
-    y = scales::rescale(rnorm(n_points), to = c(x_min, x_max))) |>
-    dplyr::mutate(
-      colour_hex = sample(palette, dplyr::n(), replace = TRUE))
-  
-  return(df)
-  
-}
+library(magick)
+# Also requires {tibble}, {ggforce}, {ggpattern}, {ggfx}, {png}, {grid}
 
 # Modifiable parameters --------------------------------------------------------
 
 seed <- 9210
-bg_colour <- "#040D10"
-glow_hex <- "#F6F1D1"
-palette <- c("#07151A", "#09191F", "#0A1D23", "#0D242B", "#132E36")
+bg_colour <- "#011928"
+palette_speckles <- c("#010D14", "#011927", "#033758", "#DE3817", "#F1917E", "#FBE0DA")
 
 # Create data ------------------------------------------------------------------
 
+# Square for gradient fill
 df_square <- tibble::tibble(
-  x = c(0,0,10,10),
-  y = c(0,10,10,0))
+  x = c(-2,-2,12,12),
+  y = c(-2,12,12,-2))
 
-df_blotches <- blotchy(
-  seed = seed, n_points = 4000, palette = palette,
-  x_min = -4, x_max = 14, y_min = -4, y_max = 14)
+# Polygon cutout for square gradient
+set.seed(seed)
+df_polygon_cutout <- tibble::tibble(
+  x = c(
+    sample(seq(-1,2.5, by = 0.2), 3),
+    sample(seq(6,7.5, by = 0.2), 3)),
+  y = c(
+    sample(seq(0,2, by = 0.2), 1),
+    sample(seq(4.5,5.5, by = 0.2), 1),
+    sample(seq(6,7.5, by = 0.2), 2),
+    sample(seq(4.5,5.5, by = 0.2), 1),
+    sample(seq(0,2, by = 0.2), 1)))
+
+# Split gradient - left side
+df_polygon_left <- tibble::tibble(
+  x = c(-2,-2,5,12,12),
+  y = c(-2,12,12,0,-2))
+
+# Split gradient - right side
+df_polygon_right <- tibble::tibble(
+  x = c(5,12,12),
+  y = c(12,12,0))
+
+# Circle cutout for split gradient
+set.seed(seed)
+df_circle_cutout <- tibble::tibble(
+  x = sample(seq(6,8, by = 0.2), 1),
+  y = sample(seq(5,7, by = 0.2), 1),
+  r = sample(seq(1,3, by = 0.5), 1))
 
 # Build plot -------------------------------------------------------------------
 
-ggplot() +
-  ggfx::with_blur(
-    geom_polygon(
-      data = df_square,
-      aes(x = x, y = y),
-      fill = glow_hex),
-    sigma = 50) +
+poly <- ggplot() +
   ggfx::as_reference(
     geom_polygon(
+      data = df_polygon_cutout,
+      aes(x = x, y = y)),
+    id = "polygon") +
+  ggfx::with_blend(
+    ggpattern::geom_polygon_pattern(
       data = df_square,
       aes(x = x, y = y),
-      fill = bg_colour),
-    id = "base") +
-  ggfx::with_blend(
-    geom_point(
-      data = df_blotches,
-      aes(x = x, y = y, colour = colour_hex),
-      shape = 16, size = 30, alpha = 0.1),
-    bg_layer = "base",
+      pattern = "gradient", pattern_fill = "#033758", pattern_fill2 = "#B0DFFC",
+      pattern_orientation = "vertical", fill = NA, colour = NA),
+    bg_layer = "polygon",
     blend_type = "atop") +
-  scale_colour_identity() +
-  coord_cartesian(xlim = c(-4,14), ylim = c(-4,14), expand = FALSE) +
+  coord_cartesian(xlim = c(-2,12), ylim = c(-2,12), expand = FALSE) +
+  theme_void()
+
+circle <- ggplot() +
+  ggforce::geom_circle(
+    data = df_circle_cutout,
+    aes(x0 = x, y0 = y, r = r),
+    fill = "#000000", colour = NA) +
+  coord_cartesian(xlim = c(-2,12), ylim = c(-2,12), expand = FALSE) +
   theme_void() +
   theme(
-    legend.position = "none",
-    plot.background = element_rect(fill = bg_colour, colour = bg_colour))
+    plot.background = element_rect(fill = "#FFFFFF", colour = "#FFFFFF"))
 
-# Save to file -----------------------------------------------------------------
+gradients_for_circle <- ggplot() +
+  ggpattern::geom_polygon_pattern(
+    data = df_polygon_left,
+    aes(x = x, y = y),
+    pattern = "gradient", pattern_fill = "#F9D0C8", pattern_fill2 = "#DE3817",
+    pattern_orientation = "horizontal", fill = NA, colour = NA) +
+  ggpattern::geom_polygon_pattern(
+    data = df_polygon_right,
+    aes(x = x, y = y),
+    pattern = "gradient", pattern_fill = "#370E06", pattern_fill2 = "#DE3817",
+    pattern_orientation = "horizontal", fill = NA, colour = NA) +
+  coord_cartesian(xlim = c(-2,12), ylim = c(-2,12), expand = FALSE) +
+  theme_void()
+
+# Save elements to file
+ggsave(
+  here::here("img/ingredients/20230130_poly.png"), poly,
+  width = 10, height = 10, units = "in", dpi = 600)
 
 ggsave(
-  here::here("img/20230130.png"), last_plot(),
-  width = 10, height = 10, units = "cm", dpi = 600)
+  here::here("img/ingredients/20230130_circle.png"), circle,
+  width = 10, height = 10, units = "in", dpi = 600)
+
+ggsave(
+  here::here("img/ingredients/20230130_grad_circle.png"), gradients_for_circle,
+  width = 10, height = 10, units = "in", dpi = 600)
+
+# Shift to {magick} workflow ---------------------------------------------------
+
+img_poly <- image_read(here::here("img/ingredients/20230130_poly.png"))
+
+img_circle <- image_read(here::here("img/ingredients/20230130_circle.png"))
+
+img_grad_circle <- image_read(here::here("img/ingredients/20230130_grad_circle.png"))
+
+# Blend gradient with circle
+img_circle_blend <- image_flatten(
+  c(img_grad_circle, img_circle), "Screen") %>%
+  image_transparent("#FFFFFF")
+
+# Blend circle gradient with polygon gradient
+img_combined <- image_flatten(
+  c(img_poly, img_circle_blend), "Over") %>%
+  image_transparent("#000000") %>%
+  image_noise(noisetype = "Laplacian") %>%
+  image_noise(noisetype = "Laplacian") %>%
+  image_noise(noisetype = "Laplacian")
+
+image_write(
+  img_combined,
+  path = here::here("img/ingredients/20230130_combined.png"))
+
+# Shift back to {ggplot2} workflow ---------------------------------------------
+
+# Read in combined, gradient-filled shapes
+fg <- png::readPNG(here::here("img/ingredients/20230130_combined.png"))
+fg_raster <- grid::rasterGrob(fg, width = unit(1,"npc"), height = unit(1,"npc"))
+
+# Create dusty speckles
+set.seed(seed)
+df_dust <- tibble::tibble(
+  x = scales::rescale(rnorm(700), to = c(-12,22)),
+  y = scales::rescale(rnorm(700), to = c(-12,22))) %>%
+  mutate(
+    colour_hex = sample(palette_speckles, n(), replace = TRUE),
+    point_size = sample(seq(0.2,0.4, by = 0.05), n(), replace = TRUE))
+
+# Build plot
+final <- ggplot() +
+  ggpattern::geom_polygon_pattern(
+    data = df_square,
+    aes(x = x, y = y),
+    pattern = "gradient", pattern_fill = "#131313", pattern_fill2 = "#02253C",
+    pattern_orientation = "vertical", fill = NA, colour = NA) +
+  annotation_custom(fg_raster, -Inf, Inf, -Inf, Inf) +
+  geom_point(
+    data = df_dust,
+    aes(x = x, y = y, colour = colour_hex, size = point_size),
+    shape = 46) +
+  scale_colour_identity() +
+  scale_size_identity() +
+  coord_cartesian(xlim = c(-2,12), ylim = c(-2,12), expand = FALSE) +
+  theme_void()
+
+# Save final image to file
+ggsave(
+  here::here("img/20230130.png"), final,
+  width = 10, height = 10, units = "in", dpi = 600)
